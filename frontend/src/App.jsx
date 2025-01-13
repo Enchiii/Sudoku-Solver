1548fe404f582b9784bad20ce12736c0e50f61f6
Enchi <eryktobiasz2006@gmail.com>
2025-01-14 00:34:16 +0100

    little improved

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import Grid from "./components/Grid.jsx";
 import Popup from "./components/Popup.jsx";
 import { getCookie, setCookie } from "./utils/cookiesUtils.jsx";
